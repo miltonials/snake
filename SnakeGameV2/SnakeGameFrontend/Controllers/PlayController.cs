fd5f5c8c83ca2c08c3c6ee5517e72f7bf9c6e5bf
Andy Porras <andyporras@estudiantec.cr>
2023-11-24 22:05:51 -0600

    Generar Tablero

---
+++ SnakeGameV2/SnakeGameFrontend/Controllers/PlayController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SnakeGameFrontend.Controllers
+{
+    public class PlayController : Controller
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        public static IConfiguration _configuration;
+        public PlayController(IConfiguration configuration, IHttpContextAccessor contextAccessor)
+        {
+            _configuration = configuration;
+            _contextAccessor = contextAccessor;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Jugador = AuthController.GetPlayerSession(_contextAccessor);
+            //conseguir numero de jugadores
+            ViewBag.CantidadJugadores = 2;
+            return View();
+        }
+    }
+}
