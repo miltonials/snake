ab95e176561822d67ec7770c75fecf13623f9ea4
Milton Barrera <miltonjosue2001@gmail.com>
2023-11-26 02:02:21 -0600

    hub de signalR para partidas

---
+++ SnakeGameV2/SnakeGameFrontend/Models/PlayHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SnakeGameBackend.Models;
+
+namespace SnakeGameFrontend.Models
+{
+    public class PlayHub : Hub
+    {
+        private async Task RemovePlayer(string room, string nickname)
+        {
+            string apiUrl = Controllers.ChatController._configuration.GetValue<string>("apiUrl");
+
+            using var httpClient = new HttpClient();
+            string encodedRoom = Uri.EscapeDataString(room);
+            string encodedNickname = Uri.EscapeDataString(nickname);
+
+            string url = $"{apiUrl}/AbandonarPartida?identificadorPartida={room}&nickname={encodedNickname}";
+            using var response = await httpClient.PostAsync(url, null);
+        }
+
+        //metodos de la partida para ./play
+        public async Task PrepararPartida(string room) {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("CargarDatosJugador");
+        }
+    }
+}
